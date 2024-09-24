ac34db140c14e0466a3e7ed339f8cf56a6cc81ac
a-hansell <auhansel@calpoly.edu>
2024-09-24 19:21:24 +0200

    Add Home and Login Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+// import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_fall_2024/screens/home/home_screen.dart';
+import 'package:todo_fall_2024/screens/login/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -16,9 +23,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Material(
-        child: Center(
-            child: Text('Hello World!'),
+      home: const SafeArea(
+        child: Scaffold(
+          body: HomeScreen(),
         ),
       ),
     );
---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sign_in_button/sign_in_button.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: SizedBox(
+          height: 48,
+          child: SignInButton(
+            Buttons.google,
+            text: "Sign up with Google",
+            onPressed: () {},
+          ),
+        )
+    );
+  }
+}
